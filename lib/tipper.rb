ee1c4aaafffb81c44d5ae835b6e0e55d33ac76d8
Elvin G. Marmol <egm0921@gmail.com>
2015-02-10 21:44:03 -0500

    Extracts tax, discount, and tip amounts from complex expression

--- lib/tipper.rb
+++ lib/tipper.rb
@@ -8,7 +8,11 @@ class Tipper
   end
 
   def total
-    amount + (amount * TAX) - (amount * (discount_percentage / 100.0)) + (amount * (tip_percentage / 100.0))
+    tax = amount * TAX
+    discount = amount * (discount_percentage / 100.0)
+    tip = amount * (tip_percentage / 100.0)
+    
+    amount + tax - discount + tip
   end
 
   private
